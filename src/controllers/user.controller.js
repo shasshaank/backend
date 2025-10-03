5659952c0f00aa1204169d99871de9232ac7ad10
shank <shashaankjain07@gmail.com>
2025-10-03 17:16:02 +0530

    Add registerd users and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,60 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
+import {apiError} from '../utils/apiError.js'
+import {User} from '../models/user.model.js'
+import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import {apiResponse} from '../utils/apiResponse.js'
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"testing"
+    const {username,password,email,fullName} = req.body
+    console.log("Email: ",email);
+
+    if([fullName,username,email,password].some((field)=>field?.trim()==="")){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username },{ email }]
     })
+
+    if(existedUser){
+        throw new apiError(409, "User with email or password already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.avatar[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new apiError(400,"Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new apiError(400,"Avatar file is required");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url||"",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new apiError(500,"Something went wrong while registering the user")
+    }
+
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
 })
 
 export {registerUser}
