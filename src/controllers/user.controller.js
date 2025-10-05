60a11bf6ea0870eb03c090c6644dd141e8b426f4
shank <shashaankjain07@gmail.com>
2025-10-05 19:03:11 +0530

    add access and refresh token reload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {apiError} from '../utils/apiError.js'
 import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {apiResponse} from '../utils/apiResponse.js'
+import jwt from 'jsonwebtoken'
 
 const generateAccessAndRefreshTokens = async(userId)=>{
     try{
@@ -153,5 +154,49 @@ const logoutUser = asyncHandler(async (req,res)=>{
     
 })
 
-export {registerUser,loginUser,logoutUser}
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+   try{
+        const incomingRefreshToken =  req.cookies.refreshToken || req.body.refreshToken
+
+        if(!incomingRefreshToken){
+                throw new apiError(401, "Unauthorized request");
+        }
+
+
+        const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+
+        const user = User.findById(decodedToken?._id);
+
+        if(!user){
+            throw new apiError(401, "Invalid refresh token")
+        }
+
+        if(incomingRefreshToken !== user?.refreshToken){
+                throw new apiError(401, "Refresh token is expired or used")
+        }
+
+        const options = {
+                httpOnly:true,
+                secure: true
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken",options)
+        .cookie("refreshToken",options)
+        .json(
+                new apiResponse(
+                    200,
+                    {accessToken, refreshToken: newRefreshToken},
+                    "Access token refreshed"
+                )
+        )
+    }catch(error){
+        throw new apiError(401, error?.message||"Invalid refresh Token ")
+    }
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken}
 
