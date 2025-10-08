7e988818fac6816ebbe8da74e873a28037b05d98
shank <shashaankjain07@gmail.com>
2025-10-08 15:06:26 +0530

    bug fixes and route updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -131,8 +131,8 @@ const logoutUser = asyncHandler(async (req,res)=>{
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set:{
-                refreshToken: undefined
+            $unset:{
+                refreshToken: 1
             }
         },
         {
@@ -198,7 +198,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     }
 })
 
-const currentPassword = asyncHandler(async(req,res)=>{
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
     const {oldPassword, newPassword} = req.body
 
     const user = await User.findById(req.user?._id)
@@ -375,18 +375,68 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     )
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user_id)
 
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200,user[0].watchHistory,"Watch history fetched successfully")
+    )
+})
 
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken, 
-    currentPassword, 
+    changeCurrentPassword, 
     getCurrentUser, 
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
 
